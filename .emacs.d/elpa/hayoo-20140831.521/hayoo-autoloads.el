a2d45082d135322d639474999fb5389c633417ad
Jun Han (Johnson) Ooi <ojh5065@gmail.com>
2018-06-22 00:03:05 -0400

    Not using Emacs anymore.

--- .emacs.d/elpa/hayoo-20140831.521/hayoo-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; hayoo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "hayoo" "hayoo.el" (21724 1452 0 0))
-;;; Generated autoloads from hayoo.el
-
-(autoload 'hayoo-query "hayoo" "\
-Do a Hayoo search for QUERY.
-
-\(fn QUERY)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hayoo-autoloads.el ends here
--- .emacs.d/elpa/textile-mode-20120721.1258/textile-mode-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; textile-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("textile-mode.el") (21724 1423 483365
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; textile-mode-autoloads.el ends here
