53d0f5d5193bfea6682307f5558d6e97f034c3a7
Jun Han (Johnson) Ooi <ojh5065@gmail.com>
2024-04-17 23:01:33 -0400

    Switch to LazyVim

--- .config/nvim/lua/plugins/nerdtree.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_command("let NERDTreeIgnore = ['\\.pyc$']")
-vim.api.nvim_command("let g:NERDTreeWinPos = 'left'")
-vim.api.nvim_command("let g:NERDTreeWinSize=35")
-vim.api.nvim_set_keymap("", "<C-n>", ":NERDTreeToggle<CR>", {noremap=false, silent=false})
--- .config/nvim/lua/plugins/vim-smoothie.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_command("let g:smoothie_no_default_mappings = v:true")
-
-local opts = {
-    noremap = false,
-    silent = false,
-}
-
-vim.api.nvim_set_keymap("n", "<C-d>", "<Plug>(SmoothieDownwards)", opts)
-vim.api.nvim_set_keymap("n", "<C-e>", "<Plug>(SmoothieUpwards)", opts)
